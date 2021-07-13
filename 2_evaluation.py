ca02369cb610bd021713ccd824a5c077ea7748a3
Cai Wingfield <cai@caiwingfield.net>
2021-07-13 17:12:00 +0100

    Fix a bug which caused immediate NO decisions.

--- 2_evaluation.py
+++ 2_evaluation.py
@@ -23,7 +23,7 @@ from copy import deepcopy
 from enum import Enum
 from logging import getLogger, basicConfig, INFO
 from pathlib import Path
-from typing import Optional, List, Tuple, Dict
+from typing import List, Tuple, Dict
 
 from pandas import read_csv, DataFrame
 
@@ -51,8 +51,23 @@ THRESHOLDS = [0.0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1.0]  # linspace was caus
 
 
 class Decision(Enum):
+    # Above yes
     Yes = 1
+    # Below no
     No = 0
+    # In the indecisive region
+    Undecided = -1
+    # Hasn't yet entered the indecisive region
+    Waiting = -2
+
+    @property
+    def made(self) -> bool:
+        """Has a decision been positively made?"""
+        if self == Decision.Yes:
+            return True
+        if self == Decision.No:
+            return True
+        return False
 
 
 class _Decider:
@@ -67,63 +82,58 @@ class _Decider:
         self.threshold_yes: ActivationValue = threshold_yes
         self.threshold_no: ActivationValue = threshold_no
 
-        # The current level of activation. None when no activation has historically been presented.
-        self._last_activation: Optional[ActivationValue] = None
+        self.last_decision: Decision = Decision.Waiting
 
-    @property
-    def _above_yes(self) -> Optional[bool]:
-        """True whenn activation is above the yes threshold (None for None)."""
-        if self._last_activation is None:
-            return None
-        return self._last_activation >= self.threshold_yes
+    def _above_yes(self, activation) -> bool:
+        """True whenn activation is above the yes threshold."""
+        return activation >= self.threshold_yes
 
-    @property
-    def _below_no(self) -> Optional[bool]:
-        """True whenn activation is below the no threshold (None for None)."""
-        if self._last_activation is None:
-            return None
-        return self._last_activation <= self.threshold_no
+    def _below_no(self, activation) -> bool:
+        """True whenn activation is below the no threshold."""
+        return activation <= self.threshold_no
 
-    @property
-    def _current_decision(self) -> Optional[Decision]:
-        """The current decision, based on the current level of activation. None if no decision has been reached."""
-        if self._above_yes:
+    def _make_decision(self, activation) -> Decision:
+        """The current decision, based on the current level of activation."""
+        # If we peak above YES in any scenario, that is a yes
+        if self._above_yes(activation):
             return Decision.Yes
-        if self._below_no:
-            return Decision.No
-        return None
-
-    def test_activation_level(self, activation: ActivationValue) -> Optional[Decision]:
-        """Present a level of activation to test if it causes a decision to be reached."""
-        previous_decision = self._current_decision
-        self._last_activation = activation
-        new_decision = self._current_decision
-        if new_decision is not previous_decision:
-            return new_decision
+        # We can only decide "no" if we were previously undecided
+        elif self.last_decision == Decision.Undecided:
+            if self._below_no(activation):
+                return Decision.No
+            else:
+                return Decision.Undecided
+        elif self.last_decision == Decision.Waiting:
+            if self._below_no(activation):
+                return Decision.Waiting
+            else:
+                return Decision.Undecided
         else:
-            return None
+            raise RuntimeError()
+
+    def test_activation_level(self, activation: ActivationValue) -> Decision:
+        """Present a level of activation to test if it causes a decision to be reached."""
+        decision = self._make_decision(activation)
+        self.last_decision = decision
+        return decision
 
     @classmethod
-    def combined_decision(cls, deciders: List[_Decider], activations: List[ActivationValue]) -> Optional[Tuple[Decision, int]]:
+    def multi_tests(cls, deciders: List[_Decider], activations: List[ActivationValue]) -> List[Decision]:
         """
-        Present each activation to each of a list of deciders, and output the decision and index of the ordinally first
-        one to make a decision.
-        None if no decision.
+        Present each activation to each of a list of deciders, and output the decision from each.
         """
-        assert len(deciders) > 0
-        assert len(deciders) == len(activations)
+        assert 0 < len(deciders) == len(activations)
+        decisions = []
         i: int
         d: _Decider
         for i, d in enumerate(deciders):
             decision = d.test_activation_level(activations[i])
-            if decision is not None:
-                return decision, i
-        return None
+            decisions.append(decision)
+        return decisions
 
 
-def make_model_decision(object_label, decision_threshold_no, decision_threshold_yes, cv_item_data, model_data, spec) -> Tuple[Optional[Decision], Optional[int]]:
-    object_label_sensorimotor: str = apply_substitution_if_available(object_label,
-                                                                     cv_item_data.substitutions_sensorimotor)
+def make_model_decision(object_label, decision_threshold_no, decision_threshold_yes, cv_item_data, model_data, spec) -> Tuple[Decision, int]:
+    object_label_sensorimotor: str = apply_substitution_if_available(object_label, cv_item_data.substitutions_sensorimotor)
     object_label_linguistic: str = apply_substitution_if_available(object_label, cv_item_data.substitutions_linguistic)
     object_label_linguistic_multiword_parts: List[str] = decompose_multiword(object_label_linguistic)
     sensorimotor_decider = _Decider(threshold_yes=decision_threshold_yes, threshold_no=decision_threshold_no)
@@ -131,42 +141,25 @@ def make_model_decision(object_label, decision_threshold_no, decision_threshold_
         _Decider(threshold_yes=decision_threshold_yes, threshold_no=decision_threshold_no)
         for _part in object_label_linguistic_multiword_parts
     ]
-    decision_made = False
-    decision = None
-    time = None
     for tick in range(spec.soa_ticks + 1, spec.run_for_ticks):
         sensorimotor_decision = sensorimotor_decider.test_activation_level(
             activation=model_data[OBJECT_ACTIVATION_SENSORIMOTOR_f.format(object_label_sensorimotor)].loc[tick])
-        linguistic_decision = _Decider.combined_decision(
+        linguistic_decisions = _Decider.multi_tests(
             deciders=linguistic_deciders,
             activations=[
                 model_data[OBJECT_ACTIVATION_LINGUISTIC_f.format(part)].loc[tick]
                 for part in object_label_linguistic_multiword_parts
             ])
-
-        if linguistic_decision is None and sensorimotor_decision is None:
-            continue
-
-        if linguistic_decision == Decision.No or sensorimotor_decision == Decision.No:
-            decision_made = True
-            decision = Decision.No
-            time = tick
-            break
-        if linguistic_decision == Decision.Yes or sensorimotor_decision == Decision.Yes:
-            decision_made = True
-            decision = Decision.Yes
-            time = tick
-            break
-    if not decision_made:
-        decision = None
-        time = None
-    return decision, time
+        for decision in [sensorimotor_decision, *linguistic_decisions]:
+            if decision.made:
+                return decision, tick
+    return Decision.Undecided, spec.run_for_ticks
 
 
-def check_decision(decision: Optional[Decision], category_verification_correct: bool) -> bool:
+def check_decision(decision: Decision, category_verification_correct: bool) -> bool:
     """Checks a Decision to see if it was correct. Returns True iff the deciion is correct."""
-    # When it's undecided, we default to no
-    if decision is None:
+    # When it's undecided or waiting, we default to no
+    if (decision == Decision.Undecided) or (decision == Decision.Waiting):
         decision = Decision.No
     if category_verification_correct:
         return decision == Decision.Yes
@@ -188,8 +181,8 @@ def hitrate_for_thresholds(all_model_data: Dict[Tuple[str, str], DataFrame],
 
         category_verification_correct: bool = CV_ITEM_DATA.is_correct(category_label, object_label)
 
-        model_decision: Optional[Decision]
-        decision_made_at_time: Optional[int]
+        model_decision: Decision
+        decision_made_at_time: int
         try:
             model_data = all_model_data[(category_label, object_label)]
         # No model output was saved
@@ -230,7 +223,7 @@ def main(spec: CategoryVerificationJobSpec):
 
     # Only load the model data once, then just reference it for each hitrate.
     # TODO: This is turning into spaghetti code, but let's get it working first.
-    logger.info(f"Loading model activation logs from {model_output_dir.as_posix()}")
+    logger.info(f"\tLoading model activation logs from {model_output_dir.as_posix()}")
     # (object, item) -> model_data
     all_model_data: Dict[Tuple[str, str], DataFrame] = dict()
     for category_label, object_label in CV_ITEM_DATA.category_object_pairs():
@@ -257,6 +250,8 @@ def main(spec: CategoryVerificationJobSpec):
         DataFrame.from_records(hitrates,
                                columns=["Decision threshold (no)", "Decision threshold (yes)", "hitrate"],
                                ).to_csv(f, header=True, index=False)
+
+    logger.info(f"Largest hitrate this model: {max(t[2] for t in hitrates)}")
 
 
 if __name__ == '__main__':
