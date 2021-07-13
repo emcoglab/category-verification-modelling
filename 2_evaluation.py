026cd0a1b9bb1ade416957be64cd89e1f0359098
Cai Wingfield <cai@caiwingfield.net>
2021-07-13 11:44:28 +0100

    Compute hitrates over many threshold pairs

--- 2_evaluation.py
+++ 2_evaluation.py
@@ -26,6 +26,7 @@ from pathlib import Path
 from typing import Optional, List, Tuple
 
 from pandas import read_csv, DataFrame
+from numpy import linspace
 
 from framework.cli.job import CategoryVerificationJobSpec
 from framework.cognitive_model.basic_types import ActivationValue
@@ -116,55 +117,107 @@ class _Decider:
         return None
 
 
-def main(spec: CategoryVerificationJobSpec, decision_threshold_yes: ActivationValue, decision_threshold_no: ActivationValue):
+def make_model_decision(object_label, decision_threshold_no, decision_threshold_yes, cv_item_data, model_data, spec) -> Optional[Decision]:
+    object_label_sensorimotor: str = apply_substitution_if_available(object_label,
+                                                                     cv_item_data.substitutions_sensorimotor)
+    object_label_linguistic: str = apply_substitution_if_available(object_label, cv_item_data.substitutions_linguistic)
+    object_label_linguistic_multiword_parts: List[str] = decompose_multiword(object_label_linguistic)
+    sensorimotor_decider = _Decider(threshold_yes=decision_threshold_yes, threshold_no=decision_threshold_no)
+    linguistic_deciders = [
+        _Decider(threshold_yes=decision_threshold_yes, threshold_no=decision_threshold_no)
+        for _part in object_label_linguistic_multiword_parts
+    ]
+    decision_made = False
+    decision = None
+    for tick in range(spec.soa_ticks + 1, spec.run_for_ticks):
+        sensorimotor_decision = sensorimotor_decider.test_activation_level(
+            activation=model_data[OBJECT_ACTIVATION_SENSORIMOTOR_f.format(object_label_sensorimotor)].loc[tick])
+        linguistic_decision = _Decider.combined_decision(
+            deciders=linguistic_deciders,
+            activations=[
+                model_data[OBJECT_ACTIVATION_LINGUISTIC_f.format(part)].loc[tick]
+                for part in object_label_linguistic_multiword_parts
+            ])
+
+        if linguistic_decision is None and sensorimotor_decision is None:
+            continue
+
+        if linguistic_decision == Decision.No or sensorimotor_decision == Decision.No:
+            decision_made = True
+            decision = Decision.No
+            break
+        if linguistic_decision == Decision.Yes or sensorimotor_decision == Decision.Yes:
+            decision_made = True
+            decision = Decision.Yes
+            break
+    if not decision_made:
+        decision = None
+    return decision
+
+
+def check_decision(decision: Optional[Decision], category_verification_correct: bool) -> bool:
+    """Checks a Decision to see if it was correct. Returns True iff the deciion is correct."""
+    # When it's undecided, we default to no
+    if decision is None:
+        decision = Decision.No
+    if category_verification_correct:
+        return decision == Decision.Yes
+    else:
+        return decision == Decision.No
+
+
+def hitrate_for_thresholds(decision_threshold_yes: ActivationValue, decision_threshold_no: ActivationValue,
+                           spec: CategoryVerificationJobSpec):
 
     cv_item_data: CategoryVerificationItemData = CategoryVerificationItemData()
 
     model_output_dir = Path(root_input_dir, spec.output_location_relative())
 
+    logger.info(f"Loading model activation logs from {model_output_dir.as_posix()}")
+
+    model_correct_count: int = 0
+    model_total_count: int = 0
     for category_label, object_label in cv_item_data.category_object_pairs():
+        model_total_count += 1
         model_output_path = Path(model_output_dir, f"{category_label}-{object_label}.csv")
         if not model_output_path.exists():
             logger.warning(f"{model_output_path.name} not found.")
             continue
 
-        object_label_sensorimotor: str = apply_substitution_if_available(object_label, cv_item_data.substitutions_sensorimotor)
-        object_label_linguistic: str = apply_substitution_if_available(object_label, cv_item_data.substitutions_linguistic)
-        object_label_linguistic_multiword_parts: List[str] = decompose_multiword(object_label_linguistic)
-
         model_data: DataFrame = read_csv(model_output_path, header=0, index_col=CLOCK, dtype={CLOCK: int})
 
-        sensorimotor_decider = _Decider(threshold_yes=decision_threshold_yes, threshold_no=decision_threshold_no)
-        linguistic_deciders = [
-            _Decider(threshold_yes=decision_threshold_yes, threshold_no=decision_threshold_no)
-            for _part in object_label_linguistic_multiword_parts
-        ]
+        category_verification_correct: bool = cv_item_data.is_correct(category_label, object_label)
 
-        decision_made = False
-        for tick in range(spec.soa_ticks + 1, spec.run_for_ticks):
-            sensorimotor_decision = sensorimotor_decider.test_activation_level(
-                activation=model_data[OBJECT_ACTIVATION_SENSORIMOTOR_f.format(object_label_sensorimotor)].loc[tick])
-            linguistic_decision = _Decider.combined_decision(
-                deciders=linguistic_deciders,
-                activations=[
-                    model_data[OBJECT_ACTIVATION_LINGUISTIC_f.format(part)].loc[tick]
-                    for part in object_label_linguistic_multiword_parts
-                ])
+        decision: Optional[Decision] = make_model_decision(object_label,
+                                                           decision_threshold_no, decision_threshold_yes,
+                                                           cv_item_data, model_data,
+                                                           spec)
 
-            if linguistic_decision is None and sensorimotor_decision is None:
+        model_correct: bool = check_decision(decision, category_verification_correct)
+        if model_correct:
+            model_correct_count += 1
+
+    model_hitrate = model_correct_count / model_total_count
+    return model_hitrate
+
+
+def main(spec: CategoryVerificationJobSpec):
+
+    hitrates = []
+    for decision_threshold_no in linspace(0, 1, 11):
+        for decision_threshold_yes in linspace(0, 1, 11):
+            if decision_threshold_no >= decision_threshold_yes:
                 continue
+            hitrate = hitrate_for_thresholds(decision_threshold_yes=decision_threshold_yes,
+                                             decision_threshold_no=decision_threshold_no,
+                                             spec=spec)
+            hitrates.append((decision_threshold_no, decision_threshold_yes, hitrate))
 
-            if linguistic_decision == Decision.No or sensorimotor_decision == Decision.No:
-                logger.info(f"{category_label}-{object_label}: {tick} NO!")
-                decision_made = True
-                break
-            if linguistic_decision == Decision.Yes or sensorimotor_decision == Decision.Yes:
-                logger.info(f"{category_label}-{object_label}: {tick} YES!")
-                decision_made = True
-                break
-
-        if not decision_made:
-            logger.info(f"{category_label}-{object_label}: UNDECIDED!")
+    model_output_dir = Path(root_input_dir, spec.output_location_relative())
+    with Path(model_output_dir, " hitrates.csv").open("w") as f:
+        DataFrame.from_records(hitrates,
+                               columns=["Decision threshold (no)", "Decision threshold (yes)", "hitrate"],
+                               ).to_csv(f, header=True, index=False)
 
 
 if __name__ == '__main__':
@@ -172,8 +225,8 @@ if __name__ == '__main__':
     logger.info("Running %s" % " ".join(sys.argv))
 
     loaded_specs = CategoryVerificationJobSpec.load_multiple(Path(Path(__file__).parent,
-                                                                 "job_specifications",
-                                                                 "2021-06-25 search for more sensible parameters.yaml"))
+                                                                  "job_specifications",
+                                                                  "2021-06-25 search for more sensible parameters.yaml"))
     systematic_cca_test = True
 
     if systematic_cca_test:
@@ -189,8 +242,6 @@ if __name__ == '__main__':
         specs = loaded_specs
 
     for spec in specs:
-        main(spec=spec,
-             decision_threshold_no=0.5,
-             decision_threshold_yes=0.9)
+        main(spec=spec)
 
     logger.info("Done!")
