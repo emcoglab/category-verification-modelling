567dc026d193cc79456feeb31830ed1011073673
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2022-01-24 13:50:59 +0000

    More jobs

--- 2_evaluation.py
+++ 2_evaluation.py
@@ -168,6 +168,7 @@ if __name__ == '__main__':
         "2021-06-25 search for more sensible parameters.yaml",
         "2021-09-07 Finer search around a good model.yaml",
         "2021-09-14 Finer search around another good model.yaml",
+        "2022-01-24 More variations on the current favourite.yaml",
     ]:
         loaded_specs.extend([(s, sfn, i) for i, s in enumerate(CategoryVerificationJobSpec.load_multiple(
             Path(Path(__file__).parent, "job_specifications", sfn)))])
